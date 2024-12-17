71f88c729bcb1d0705d20a5c65deeedbb803e048
Victor <rodrigues.vmm@gmail.com>
2024-12-17 14:52:48 +0100

    ajout TP Data-binding ajout nfIf

--- src/app/list-friends/list-friends.component.ts
+++ src/app/list-friends/list-friends.component.ts
@@ -2,23 +2,31 @@ import { Component } from '@angular/core';
 import { OneFriendComponent } from './one-friend/one-friend.component';
 import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-list-friends',
-  imports: [ OneFriendComponent, FormsModule ],
+  imports: [ OneFriendComponent, FormsModule , NgIf ],
   templateUrl: './list-friends.component.html',
   styleUrl: './list-friends.component.css'
 })
 export class ListFriendsComponent {
  listFriendsAuth:boolean = false;
 
+ listFriendCreationStatus:boolean = false;
+
  listFriendsCreationStatus:string = "Tu n'as pas d'ami 😭";
 
  listFriendsInputText:string="Coin Coin";
 
+ leFriend:string = "";
+
+ listFriendCreated:boolean= false;
+
  onAddingFriends():void{
-  this.listFriendsCreationStatus = "Super, tu as un ami 🥳"
+  // this.listFriendsCreationStatus = "Super, tu as un ami 🥳";
+  this.listFriendCreated = true;
  };
 
  onUpdateFriendsList(eventInput:Event):void{
