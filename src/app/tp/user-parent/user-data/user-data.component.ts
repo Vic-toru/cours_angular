7615eb1cc0f8612a308435bebb20e77eee786d06
Victor <rodrigues.vmm@gmail.com>
2025-01-20 13:47:34 +0100

    ajout TP Component Communication

--- src/app/tp/user-parent/user-data/user-data.component.ts
+++ src/app/tp/user-parent/user-data/user-data.component.ts
@@ -14,10 +14,8 @@ export class UserDataComponent {
   @Output() userUpdated = new EventEmitter<{ name: string, age: number }>();
 
   updateUserData() {
-    // Créez un objet avec les nouvelles valeurs
     const updatedUser = { name: this.name, age: this.age };
     
-    // Émet l'événement vers le parent
     this.userUpdated.emit(updatedUser);
   }
 
