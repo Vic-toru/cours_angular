aa4686f9dee6256ceeb94c9e613f130ef4aec494
Victor <rodrigues.vmm@gmail.com>
2025-01-21 16:48:29 +0100

    modif ignore

---
+++ src/environment/environment.ts
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyDS7XQXjrzBYid8GyjolHlc7qadKftZQcQ",
+    authDomain: "firegular-5c0e6.firebaseapp.com",
+    projectId: "firegular-5c0e6",
+    storageBucket: "firegular-5c0e6.firebasestorage.app",
+    databaseURL: "https://firegular-5c0e6-default-rtdb.europe-west1.firebasedatabase.app",
+    messagingSenderId: "332087524986",
+    appId: "1:332087524986:web:66093db439b1f8ca6145c3",
+    measurementId: "G-HSWLXF44X5"
+  }
+};
